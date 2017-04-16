a4b4a81235d4b3d02313bf902ace1c7c2ad24bed
AshishkrGoyal <15103140ashish@gmail.com>
2017-04-16 20:57:05 +0530

    lightbox testing

---
+++ Lightbox_Demo/script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Ashish Goyal on 4/9/2017.
+ */
+jQuery(document).ready(function () {
+    jQuery("#nanoGallery").nanoGallery({
+        thumbnailWidth: 'auto',
+        thumbnailHeight: 100,
+        locationHash: false,
+        thumbnailHoverEffect:'borderLighter,imageScaleIn80'
+        /*itemsBaseURL:'http://nanogallery.brisbois.fr/demonstration/'*/
+    });
+});
